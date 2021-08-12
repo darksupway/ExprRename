11dca75cb3a18899284174f9d95c5b028deb36d2
fb89zila <fb89zila@student.tu-freiberg.de>
2021-08-13 01:21:26 +0200

    overhaul with dicts and lists

--- script/exprrename.py
+++ script/exprrename.py
@@ -1,144 +1,176 @@
 import sys
 import os
+from pathlib import Path, PurePath
 import re
 import shutil as su
 import pandas as pd
-from pandas.core.frame import DataFrame
 
 def read_config():
     """Reads the config file in working directory, creates it from the default file or prints exeption.
 
     Returns:
-        List: Read lines if config file was successfully read. Empty if not."""
+        List: Read lines if config file was successfully read. Empty if not.
+        Bool: True if file exists/could be read. False if not."""
 
     try:
-        config_file = open("!exprrename.config", "r")
-        config = config_file.readlines()
-        config_file.close()
-        return config
+        with open("!exprrename.config") as config_file:
+            config = config_file.readlines()
+        config = [x.rstrip("\n") for x in config if not x.startswith('#')] # removin comments and newlines
+        return config, True
     except FileNotFoundError:
         print("Configuration file was not found. Creating new file from default...\n")
-        default_config_path = os.path.join(os.path.dirname(__file__), "default.config")
-        config_path = os.path.join(os.getcwd(), "!exprrename.config")
+        default_config_path = Path(__file__).parent / "default.config"
+        config_path = Path.cwd() / "!exprrename.config"
         su.copyfile(default_config_path, config_path)
+        print("The configuration file was created.")
     except Exception as ex:
         print(ex)
-    return []
+    return [], False
 
-def parse_config(conf: list[str]):
+def parse_config(config: list[str]):
     """Parse content of config file.
     
     Returns:
-        List: Renaming rules
         Pattern: The regular expression for the file names of the files which should be renamed
-        List: Groups consisting of numbers where something gets substracted"""
+        Tuple: Name group, substraction group, Output format (group1, separator, group2)
+        List: Renaming rules"""
 
-    config = [x.rstrip("\n") for x in conf]
-
-    arguments = [x for x in config if x.startswith("~ ")]
+    arguments = [x.lstrip("~ ") for x in config if x.startswith("~ ")]
     rules = [x for x in config if not x.startswith("~ ")]
 
-    if (len(arguments) >= 1):
-        file_rx = re.compile(arguments[0].lstrip("~ "))
+    if (arguments):
+        file_rx = re.compile(arguments[0])
 
         try:
-            subgroup = arguments[1].lstrip("~ ")
-            subgroup = int(subgroup)
+            namegr, subgr, gr_one, sep, gr_two = re.match("(\d), (\d), \"(\d)(.*)(\d)\"", arguments[1]).groups()
+            output = (int(namegr), int(subgr), int(gr_one), sep, int(gr_two))
         except:
-            print("Could not parse the substraction number. Replaced with zero.")
-            subgroup = 0
+            output = None
     else:
         print("No arguments given. Empty RegEx and substraction number.")
-        file_rx = re.compile('')
-        subgroup = 0
+        file_rx = None
+        output = None
     
-    return rules, file_rx, subgroup
+    return file_rx, output, rules
 
-def read_rules(rl):
+def parse_rules(rules: list[str]):
     """Parses rules and splits them in the substraction numbers, the old filename and the new filename with eventual subdir(s)
 
     Returns:
-        DataFrame: Rules split in columes: 'subnum', 'oldname' and 'newname'"""
-    rx = re.compile("(?:\[([\d,]+)\])?(.+)\|(.+)")
-    rules = [rx.match(x).groups() for x in rl if rx.match(x)]
+        Dict: Key ist 'oldname' and value is tuple ('newname','subnum')"""
 
-    rules_df = pd.DataFrame(data=rules, columns=["subnum", "oldname", "newname"])
-    rules_df["subnum"] = rules_df["subnum"].apply(lambda x: '0' if x == None else x)
-    rules_df = rules_df.astype({"subnum": int})
+    rx = re.compile("(?:\[([\d]+)\])?(.+)\|(.+)")
+    rules_groups = [rx.match(x).groups() for x in rules if rx.match(x)]
 
-    return rules_df
+    rule_dict = {}
 
-def read_filenames(rx):
+    for rule in rules_groups:
+        try:
+            rule_dict.update({rule[1]: (rule[2], int(rule[0]))})
+            #                 oldname: (newname, subnum)
+        except:
+            rule_dict.update({rule[1]: (rule[2], 0)})
+
+    return rule_dict
+
+def parse_filenames(rx: re.Pattern, subgr: int):
     """Parses files in current directory and splits filename of matches in its groups.
 
     Returns:
-        DataFrame: Matched filenames split in groups"""
-    current_dir = os.scandir()
+        List: full match, gr1, gr2, extention, len(subgr)   (subgr also converted to int)
+        
+    Exeptions:
+        SyntaxError: Raised when subgroup is over 2 or is not a digit"""
+
     files = []
-    ext = []
 
-    for element in current_dir:
-        if element.is_file():
-            files.append(element.name)
-            ext.append(element.name)
-
-    current_dir.close()
-
-    files = [x for x in files if rx.match(x)]
-    matched_files = [rx.match(x).groups() for x in files]
-    ext = [os.path.splitext(x)[1] for x in ext if rx.match(x)]
-    df = pd.DataFrame(data=matched_files, columns=range(1, len(matched_files[0])+1))
-
-    return df, files, ext
-
-def create_subnum_len_df(sg, df):
-    """Creates dataframe which contains the length of the number strings in filenames."""
-    len_df = pd.DataFrame(data=[len(x) for x in df[sg]], columns=[sg])
-    df[sg] = [int(x) for x in df[sg]]
-    return len_df
-
-def apply_rules(sg, f_df: DataFrame, r_df: DataFrame):
-    """Applies rules from config file. Substract substraction numbers and add 'newname' column in files_df."""
-    len_df = create_subnum_len_df(sg, f_df)
-    f_df["newname"] = ""
-
-    for rule in r_df.itertuples():
-        f_df.loc[f_df[1] == rule[2], sg] = f_df.loc[f_df[1] == rule[2], sg]-rule[1]
-        f_df.loc[f_df[1] == rule[2], "newname"] = rule[3]
+    for file in Path.cwd().iterdir():
+        if file.is_file():
+            files.append(file)
     
-    f_df[sg] = f_df[sg].apply(str)
+    m_files = [rx.match(x.name) for x in files if rx.match(x.name)]
+    ext = [x.suffix for x in files if rx.match(x.name)]
 
-    for i in range(len(f_df)):
-        f_df.loc[i, sg] = f_df.loc[i, sg].zfill(len_df.loc[i, sg])
+    file_list = []
 
-def rename_files(root, files, ext, f_df, sg):
-    for i in range(len(ext)):
-        new_file = f_df.loc[i, "newname"] + " - " + f_df.loc[i, sg] + ext[i]
+    for i in range(len(m_files)):
+        file_list.append([m_files[i][0], m_files[i][1], m_files[i][2], ext[i]])
         
-        if not os.path.exists(os.path.dirname(new_file)):
-            os.mkdir(os.path.dirname(new_file))
+        if (subgr <= 2 and m_files[i][subgr].isdigit()):
+            file_list[i].append(len(m_files[i][subgr]))
+            file_list[i][subgr] = int(m_files[i][subgr])
+        else:
+            raise SyntaxError("Output format in config file faulty.")
 
-        su.move(os.path.join(root, files[i]), os.path.join(root, new_file))
-        print("'{}' was moved.".format(files[i]))
+    return file_list
 
-##### Main #####
-current_dir = os.getcwd()
-configlines = read_config()
-config = [x for x in configlines if not x.startswith('#')]
+def apply_rules(rule_dict: dict, file_list: list[list], namegr: int, subgr: int):
+    """Applies rules from config file to filenames and numbers."""
+    
+    for i in range(len(file_list)):
+        file_list[i][subgr] -= rule_dict[file_list[i][namegr]][1] #substract substraction number from number
+        file_list[i][subgr] = str(file_list[i][subgr]).zfill(file_list[i][4])
+        file_list[i][namegr] = rule_dict[file_list[i][namegr]][0] #change filename to newname
 
-if (config):
-    rules, file_rx, subgroup = parse_config(config)
+def split_namegroup(newname: str):
+    """Splits directory and filename in namegroup.
+    
+    Returns:
+        str: Subdirectory(s)
+        str: Filenames"""
 
-    if (rules):
-        rules_df = read_rules(rules)
-        files_df, files, ext = read_filenames(file_rx)
-        
-        apply_rules(subgroup, files_df, rules_df)
+    newpath = Path(newname)
 
-        rename_files(current_dir, files, ext, files_df, subgroup)
+    subdir = newpath.parent
+    name = newpath.stem
+
+    return subdir, name
+
+def rename_files(file_list: list, output: tuple):
+    """Renames file with given output format. Creates subfolders if they do not exist."""
+
+    namegr, subgr, gr_one, sep, gr_two = output
+    #Name group: int, substraction group: int, Output format (group1: int, separator: str, group2: int)
+
+    for file in file_list:
+        subdir, file[namegr] = split_namegroup(file[namegr])
+
+        subdir_path = Path(subdir)
+
+        if (not subdir_path.exists()):
+            subdir_path.mkdir()
+
+        current_file_location = Path(file[0]).resolve()
+        new_file_location = subdir_path.resolve() / (file[gr_one] + sep + file[gr_two] + file[3])
+
+        su.move(current_file_location, new_file_location)
+
+############### Main ###############
+config, config_exists = read_config()
+
+if (config_exists and config):
+    file_rx, output, rules = parse_config(config)
+
+    if (rules and output):
+        rule_dict = parse_rules(rules)
+
+        try:
+            file_list = parse_filenames(file_rx, output[1])
+        except SyntaxError as ex:
+            print(ex.msg)
+            file_list = False
+
+        if (file_list):
+            # TODO: rename files
+            apply_rules(rule_dict, file_list, output[0], output[1])
+            rename_files(file_list, output)
+    elif (not rules and output):
+        print("There where no renaming rules in the configuration file. Aborting...")
+    elif (rules and not output):
+        print("There was no/a faulty output format in the configuration file. Aborting...")
     else:
-        print("There where no renaming rules in the config file. Aborting...")
-elif (configlines and not config):
+        print("There where no rules and no/a faulty output format in the configuration file. Aborting...")
+elif (config_exists and not config):
     print("The configuration file is empty or only consists of comments.")
-else:
-    print("The configuration file was created.")
+
+print("-----\n End")
